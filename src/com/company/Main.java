96288ae90760e99bbcf65f9dc21d5ea9777ab0e1
Edbergarn <31540314+Edbergarn@users.noreply.github.com>
2019-09-11 10:35:30 +0200

    Added menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int msg = Message.msg;
-        System.out.println(msg);
+        System.out.println("Välkommen! Vad vill du göra?");
+        System.out.println("1. Visa meddelanden");
+        System.out.println("2. Lägg till meddelande");
+        System.out.println("3. Uppdatera meddelande");
+        System.out.println("4. Spara meddelanden till fil");
+        System.out.println("5. läs in meddelande från fil");
+        System.out.println("6. Avsluta");
 	// write your code here
     }
 }
--- src/com/company/Message.java
+++ src/com/company/Message.java
@@ -2,11 +2,22 @@ package com.company;
 
 import java.sql.Time;
 import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 public class Message {
     private Date createdAt;
     private Date updatedAt;
     private String message;
-    private String author;
+    private String author = System.getProperty("user.name");
+
+    public Message(String msg){
+        if (msg.length() <= 140){
+            message = msg;
+        }else {
+            System.out.println("Message is too long");
+            System.exit(0);
+        }
+    }
 }
