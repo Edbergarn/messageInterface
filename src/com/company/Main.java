be24327b38e7ed89b1b236dfe29b3dff5780e943
Edbergarn <31540314+Edbergarn@users.noreply.github.com>
2019-09-11 10:06:57 +0200

    Startup, added the 4 private variables

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int msg = Message.msg;
+        System.out.println(msg);
+	// write your code here
+    }
+}
---
+++ src/com/company/Message.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class Message {
+    private Date createdAt;
+    private Date updatedAt;
+    private String message;
+    private String author;
+}
